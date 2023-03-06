db9a667e818a3b2d3001a4d54ca20adfc8b57516
Emmanuel manucho <manuchoadero@gmail.com>
2023-03-06 21:08:29 +0300

    fix url format

--- apps/utils/email_operations.py
+++ apps/utils/email_operations.py
@@ -5,14 +5,14 @@ client = EmailClient()
 
 
 def confirm_account(data: dict) -> None:
-    data["url"] = f"{settings.BACKEND_URL}/auth/confirm-email/{data['token']}/"
+    data["url"] = f"{settings.BACKEND_URL}auth/confirm-email/{data['token']}/"
     del data["token"]
     data.update({"subject": "Confirm Account", "send_to": [data["email"]]})
     client.send_email(data, "email_confirmation.html")
 
 
 def reset_password(data: dict) -> None:
-    data["url"] = f"{settings.BACKEND_URL}/auth/change-password/{data['token']}/"
+    data["url"] = f"{settings.BACKEND_URL}auth/change-password/{data['token']}/"
     del data["token"]
     data.update({"subject": "Reset Password", "send_to": [data["email"]]})
     client.send_email(data, "reset_password.html")
