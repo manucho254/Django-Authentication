c902e605671bec3fda21f85d17ebc4124709d7fc
Emmanuel manucho <manuchoadero@gmail.com>
2023-03-06 21:02:15 +0300

    added tests folder

--- apps/core/tests.py
+++
@@ -1,9 +0,0 @@
-from django.test import TestCase
-from django.test import Client
-
-# Create your tests here.
-
-class TestAuth(TestCase):
-    
-    def setUp(self) -> None:
-        self.client = Client()
--- apps/core/urls.py
+++ apps/core/urls.py
@@ -8,8 +8,16 @@ urlpatterns = [
     path("auth/register/", views.register_view, name="register"),
     path("auth/login/", views.login_view, name="login"),
     path("auth/logout/", views.logout_view, name="logout"),
-    path("auth/confirm-email/<str:token>/", views.confirm_account_view, name="confirm-email"),
-    path("auth/change-password/<str:token>/", views.change_password_view, name="change-password"),
+    path(
+        "auth/confirm-email/<str:token>/",
+        views.confirm_account_view,
+        name="confirm-email",
+    ),
+    path(
+        "auth/change-password/<str:token>/",
+        views.change_password_view,
+        name="change-password",
+    ),
     path("auth/reset-password/", views.reset_password_view, name="reset-password"),
     path("dashboard/", views.dashboard_view, name="dashboard"),
 ]
